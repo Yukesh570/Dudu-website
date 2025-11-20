4e7673b2c90da525374d49f8f18a322516840649
yukesh570 <yukesh.maha10@gmail.com>
2025-11-20 23:09:40 +0545

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { ServicesSection } from "@/components/ServiceSection";
 
 export default function HomePage() {
   return (
+    
     <main className="container mx-auto px-4">
       <ServicesSection />
     </main>
